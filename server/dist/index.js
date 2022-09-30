3f0a10ae054a83f1024f9e1b12abac4bd32a4826
vipulkumar2002 <vipulkumarpatel2002@gmail.com>
2022-09-30 23:04:02 +0530

    gitignore updated

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,22 +1,22 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 // import packages
-import dotenv from "dotenv";
-// initilize dotenv
-dotenv.config();
-import express from "express";
+require("dotenv").config(); // importing dotenv
+
+
 // import cors from "cors";
 // import helmet from "helmet";
 //Helmet helps you secure your Express apps by setting various HTTP headers
-
 //use packages
-const app = express();
-
-app.use(express.json());
-// app.use(cors());
+const app = (0, _express.default)();
+app.use(_express.default.json()); // app.use(cors());
 // app.use(helmet());
-
 //connect DB
 // import connectDB from "./database/connection";
-
 // const PORT = process.env.DEFAULT_PORT;
 // app.listen(PORT, () => {
 //   connectDB()
@@ -25,15 +25,15 @@ app.use(express.json());
 //     })
 //     .catch((err) => {
 //       console.log(err);
-
 //       console.log("Server is not running !!");
 //     });
 // });
 
 app.get("/", (req, res) => {
-  res.json({ message: "server is running." });
+  res.json({
+    message: "server is running."
+  });
 });
-
 app.listen(4000, () => {
   console.log("Server is running !!");
 });
