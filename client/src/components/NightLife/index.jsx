1489da88c53586ed9705b5f0062ac424e5614dc4
vipulkumar2002 <vipulkumarpatel2002@gmail.com>
2022-10-12 23:40:01 +0530

    NightLife Carousel done

--- client/src/components/NightLife/index.jsx
+++ client/src/components/NightLife/index.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import NightLifeCarousel from "./NightLife.Carousel";
 
 const NightLife = () => {
-  return <div>NightLife</div>;
+  return (
+    <div className="mb-10">
+      <h1 className="text-xl my-4 md:my-8 md:text-3xl md:font-semibold">
+        Nightlife Restaurant in Delhi NCR
+      </h1>
+      <NightLifeCarousel />
+    </div>
+  );
 };
 
 export default NightLife;
