339ba954c5368b9b8ec5cc80c30b5be52558a2a2
vipulkumar2002 <vipulkumarpatel2002@gmail.com>
2022-10-22 22:26:01 +0530

    UI Done !

--- client/src/layouts/Checkout.layout.jsx
+++ client/src/layouts/Checkout.layout.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import Navbar from "../components/Navbar/CheckoutNavbar";
 
 const CheckoutLayout =
   (Component) =>
   ({ ...props }) => {
     return (
       <>
-        <h1>CheckoutLayout</h1>
-        <Component {...props} />
+        <Navbar />
+        <div className="container mx-auto px-4 lg:px-20">
+          <Component {...props} />
+        </div>
       </>
     );
   };
